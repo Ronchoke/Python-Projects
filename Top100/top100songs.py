67f098577bb0ad2232a563e0d12b47eabc1c2727
Ronchoke <ronchoke@gmail.com>
2022-08-06 22:16:57 +0300

    Get Lyrics for Song Title. Add get_lyrics function.

--- Top100/top100songs.py
+++ Top100/top100songs.py
@@ -3,8 +3,8 @@ the HOT 100 index by Billboard (https://www.billboard.com/charts/hot-100/)
 plots the 100 most used words in all these songs lyrics
 and shows the top 10 artists that use the most words"""
 
-# import string
-# import matplotlib.pyplot as plt
+import string
+import matplotlib.pyplot as plt
 import requests
 
 from bs4 import BeautifulSoup as Soup
@@ -17,6 +17,7 @@ def get_full_url_to_lyrics(song_title: str, artist: str) -> str:
     :param artist: str
     :return: url: str
     """
+    # Check Input
     if type(song_title) is not str or type(artist) is not str:
         raise TypeError("Input type must be string.")
     if song_title == '':
@@ -24,44 +25,71 @@ def get_full_url_to_lyrics(song_title: str, artist: str) -> str:
     if artist == '':
         raise ValueError("Artist\'s Name must be given")
 
+    # Handle multiple Artists
     if '&' in artist:
         artist = artist.split(' & ')[0]
 
+    # Create Artist page URL address
     url = 'https://www.lyrics.com/artist/' + artist.split(' ')[0].lower()
-
     for i in range(1, len(artist.split())):
         url = url + '%20' + artist.split(' ')[i].lower()
 
+    # Connect to url and check response
     r = requests.get(url)
     if r.status_code != 200:
         raise ConnectionError
 
+    # Get artist's page content
     container = Soup(r.text, 'html.parser')
     songs = container.find_all('td', {'class': 'tal qx'})
 
+    # Non-existing artists return empty string
     if len(songs) == 0:
         raise ValueError("Artist not found, check input for typos")
 
+    # Create song's page URL
     base = 'https://www.lyrics.com'
     to_lyr = ''
     for i in range(len(songs)):
         if songs[i].a is None:
-            pass
+            continue
         elif songs[i].a.text.lower() == song_title.lower():
             to_lyr = songs[i].a.attrs['href']
         else:
-            pass
+            continue
 
+    # Unfound song titles
     if to_lyr == '':
         raise ValueError("Title not found, check song input for typos")
 
     return base + to_lyr
 
 
+def get_lyrics(song_title: str, artist: str) -> str:
+    """The get_lyrics function gets the lyrics for a song_title,
+    from https://www.lyrics.com website
+    and return all the lyrics
+
+    :param song_title: str.
+    :param artist: str
+    :return: lyrics: str
+    """
+    url = get_full_url_to_lyrics(song_title, artist)
+
+    # Connect to url and check response
+    req = requests.get(url)
+    if req.status_code != 200:
+        raise ConnectionError
+
+    # Parse for lyrics
+    lyr_page = Soup(req.text, 'html.parser')
+    return lyr_page.find('pre', {'id': 'lyric-body-text'}).text
+
+
 def __main__():
-    song_title = 'None'
+    song_title = 'Yesterday'
     artist = "the Beatles"
-    print(get_full_url_to_lyrics(song_title, artist))
+    print(get_lyrics(song_title, artist))
 
 
 if __name__ == "__main__":
